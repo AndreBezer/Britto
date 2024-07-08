d5e8eff33f50c2156b86dd9d63e480fb06f79b5f
André Luis <andrelus475@gmail.com>
2024-07-08 09:57:04 -0300

    Update codigo_full.py

--- codigo_full.py
+++ codigo_full.py
@@ -1,9 +1,10 @@
 import telebot as tb
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-chave = "6332233970:AAHDxrOHak-Uss-f19_u7-GXypqrJfYq81E"
+chave = "Sua_chave_telegram"
 bot = tb.TeleBot(chave)
 
+#Botão com redirecionador para sites
 def link():
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
@@ -11,28 +12,24 @@ def link():
         InlineKeyboardButton("Link do Mines 💣", url="https://jonbet.cxclick.com/visit/?bta=36278&brand=jonbet"), 
         InlineKeyboardButton("Link do Robo 🤖", url="https://t.me/robominespremium1")
     )
-    return markup
+    return markup 
 
+#Botão com redirecionador para canal Telegram
 def link_18():
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
     markup.add(
-        InlineKeyboardButton("Link da Putaria 🔞", url="https://t.me/BrasileirasTaradas"), 
+        InlineKeyboardButton("Link do Grupo", url="https://https://t.me/+M1ChcWWFuRVmN2Zh"), 
         )
     return markup
 
-'''@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    if call.data == "pnnormal":
-        bot.send_message(call.id, text = "<a href='https://t.me/BrasileirasTaradas'>telegeam</a>", parse_mode = ParseMode.HTML)
-    elif call.data == "pnmacho":
-        bot.answer_callback_query(call.id, "https://t.me/+TwFLuYKvt8xiMzcx")'''
-
+#Comando 'Video' envia um video para o canal
 @bot.message_handler(commands=["video"])
 def video_premiado(mensagem):
     with open('video_premiado.mp4', 'rb') as foto_premiada:
         bot.send_video(mensagem.chat.id, foto_premiada, caption='Grupo com varias Gostosas esperando por ti!', reply_markup=link_18()) # send a video with buttons
 
+#Comando 'start' envia uma foto para o canal
 @bot.message_handler(commands=["start"])
 def responder(mensagem):
     with open('foto_premiada.jpg', 'rb') as foto_premiada:
